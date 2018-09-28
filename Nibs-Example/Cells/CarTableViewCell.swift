37790aea1e7a93b2634ed0e9b4490d4514aed786
Fabio Bermudez <fabio.bermudez@sixt.com>
2018-09-28 11:32:10 +0200

    no message

---
+++ Nibs-Example/Cells/CarTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CarTableViewCell.swift
+//  Nibs-Example
+//
+//  Created by Fabio Bermudez on 9/28/18.
+//  Copyright © 2018 Fabio Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class CarTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var carNameLabel: UILabel!
+    @IBOutlet weak var carInfoLabel: UILabel!
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+
+    
+}
