f20f9aa67b2e960d8ea6ba99376b4855a64eb1e0
Fabio Bermudez <fabio.bermudez@sixt.com>
2018-09-28 10:44:01 +0200

    first commit

---
+++ Nibs-Example/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nibs-Example
+//
+//  Created by Fabio Bermudez on 9/28/18.
+//  Copyright © 2018 Fabio Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
